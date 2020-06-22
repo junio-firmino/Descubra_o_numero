2833aa688ab5ab4cd520763fb98e46099efe3f14
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-06-22 20:38:27 -0300

    Inserção de explicação no código

--- Dados_duplo.py
+++ Dados_duplo.py
@@ -2,20 +2,19 @@ import random
 
 class Dados():
     def __init__(self):
-        self.dados_min = 1
-        self.dados_max = 6
-        self.num_aletorio = 0
-        self.num_aletorio_2 = 0
+        self.dados_min = 1          # Estabelecemos o valor mínimo do dado
+        self.dados_max = 6          # Estabelecemos o valor máximo do dado
+        self.num_aletorio = 0       # Estabelecemos que essa variável comece do zero
+        self.num_aletorio_2 = 0     # Estabelecemos que essa variável comece do zero
 
-    def interface (self):
-        print("Jogo dos dados.")
+    def interface (self):           # Chamada inicial do programa
+        print("Jogo dos dados.")    
 
     def dados_aleat(self):
         self.interface()
-        self.num_aletorio = random.randint(self.dados_min,self.dados_max)
-        self.num_aletorio_2 = random.randint(self.dados_min,self.dados_max)
-
-        soma = int(self.num_aletorio) + int(self.num_aletorio_2)
+        self.num_aletorio = random.randint(self.dados_min,self.dados_max)       # Escolha dos numeros aleatórios (dado 1) 
+        self.num_aletorio_2 = random.randint(self.dados_min,self.dados_max)     # Escolha dos numeros aleatórios (dado 2)
+        soma = int(self.num_aletorio) + int(self.num_aletorio_2)                # Soma dos valores escolhidos para ajudar os jogadores
 
         print('o numero escolhido do dado 1 é: ' + str(self.num_aletorio))
         print('o numero escolhido do dado 2 é: ' + str(self.num_aletorio_2))
@@ -24,7 +23,7 @@ class Dados():
         self.final()
 
 
-    def final(self):
+    def final(self):                # Método final 
         print('Este é o nosso protótipo de dados para jogos de tabuleiro.')
 
 
